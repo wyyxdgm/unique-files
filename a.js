9bb69ff6b179a1ade9e4b3c976b9a6fc6a7d3db2
Damo <wyyxdgm@163.com>
2023-09-06 23:27:41 +0800

    delay:folder

--- a.js
+++ a.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
+// const folderPath = '/Volumes/dav_damo/baiduyun/';
 const folderPath = './folder';
 const outputFilePath = 'a.json';
 const delayInSeconds = 3; // 设置延迟的秒数
-
 function getFileStats(filePath) {
   const stats = fs.statSync(filePath);
   return {
@@ -17,32 +17,29 @@ function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function readFilesInFolder(folderPath) {
-  return new Promise((resolve, reject) => {
-    fs.readdir(folderPath, async (err, files) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      const fileDataPromises = [];
-      for (let file of files) {
-        const filePath = path.join(folderPath, file);
-        const stats = await fs.promises.stat(filePath);
-        if (stats.isDirectory()) {
-          console.log(`delay ${delayInSeconds}s for folderPath: ${filePath}`);
-          await delay(delayInSeconds * 1000);
-          fileDataPromises.push(readFilesInFolder(filePath));
-        }
-        fileDataPromises.push(getFileStats(filePath));
-      }
+async function readFilesInFolder(folderPath) {
+  try {
+    const files = await fs.promises.readdir(folderPath);
+    const filesData = [];
 
-      Promise.all(fileDataPromises)
-        .then(result => resolve(result.flat()))
-        .catch(reject);
-    });
-  });
+    for (const file of files) {
+      const filePath = path.join(folderPath, file);
+      const stats = await fs.promises.stat(filePath);
+
+      if (stats.isFile()) {
+        filesData.push(getFileStats(filePath));
+      } else if (stats.isDirectory()) {
+        filesData.push(...await readFilesInFolder(filePath));
+        console.log(`delay ${delayInSeconds}s for folderPath: ${file}`);
+        await delay(delayInSeconds * 1000); // 添加延迟
+      }
+    }
+
+    return filesData;
+  } catch (err) {
+    throw err;
+  }
 }
-
 
 readFilesInFolder(folderPath)
   .then(filesData => {
