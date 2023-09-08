b5e9136fde41cb041e2ffe3d9157afaaac7b2b35
Damo <wyyxdgm@163.com>
2023-09-08 15:27:19 +0800

    a

--- a.js
+++ a.js
@@ -5,13 +5,17 @@ const folderPath = '/Volumes/dav_damo/baiduyun/';
 // const folderPath = './folder';
 const outputFilePath = 'a.json';
 const deleteFilePath = 'd.json';
+const csvFilePath = 'a.csv';
 const delayInSeconds = 1.6; // 设置延迟的秒数
+
 function getFileStats(filePath) {
   const stats = fs.statSync(filePath);
-  return {
-    path: filePath,
-    size: stats.size
-  };
+  return [filePath, stats.size];
+}
+
+function appendLineToCSV(arr) {
+  const csvData = `${arr.join(',')}\n`;
+  fs.appendFileSync(csvFilePath, csvData); // 追加到 CSV 文件末尾
 }
 
 function delay(ms) {
@@ -19,36 +23,44 @@ function delay(ms) {
 }
 
 async function readFilesInFolder(folderPath, deleteList) {
-  try {
-    const files = await fs.promises.readdir(folderPath);
-    const filesData = [];
+  const stack = [folderPath];
+  const filesData = [];
+
+  while (stack.length > 0) {
+    const currentPath = stack.pop();
+    const files = await fs.promises.readdir(currentPath);
 
     for (const file of files) {
-      const filePath = path.join(folderPath, file);
+      const filePath = path.join(currentPath, file);
       console.log('p=', filePath);
       const stats = await fs.promises.stat(filePath);
 
       if (stats.isFile()) {
-        filesData.push(getFileStats(filePath));
+        if (!filesData.length) appendLineToCSV(['filePath', 'size']);
+        let line = getFileStats(filePath);
+        filesData.push(line);
+        appendLineToCSV(line);
       } else if (stats.isDirectory()) {
-        if (file.endsWith('.app') || file === 'node_modules') {
+        if (file.endsWith('.app') || file === 'node_modules' || file === '.git') {
           deleteList.push(filePath); // 将待删除目录添加到列表
         } else {
-          filesData.push(...await readFilesInFolder(filePath, deleteList));
           console.log(`delay ${delayInSeconds}s for folderPath: ${filePath}`);
           await delay(delayInSeconds * 1000); // 添加延迟
+          stack.push(filePath);
         }
       }
     }
-
-    return filesData;
-  } catch (err) {
-    throw err;
   }
+
+  return filesData;
 }
+
 const deleteList = [];
+const filesData = [];
 readFilesInFolder(folderPath, deleteList)
-  .then(filesData => {
+  .then(result => {
+    filesData.push(...result);
+
     fs.writeFileSync(outputFilePath, JSON.stringify(filesData, null, 2));
     console.log(`File data saved to ${outputFilePath}`);
 
@@ -58,3 +70,12 @@ readFilesInFolder(folderPath, deleteList)
   .catch(err => {
     console.error(err);
   });
+
+process.on('beforeExit', () => {
+  console.log('beforeExit');
+  if (!fs.existsSync(outputFilePath)) fs.writeFileSync(outputFilePath, JSON.stringify(filesData, null, 2));
+  console.log(`File data saved to ${outputFilePath}`);
+
+  if (!fs.existsSync(deleteFilePath)) fs.writeFileSync(deleteFilePath, JSON.stringify(deleteList, null, 2));
+  console.log(`Delete list saved to ${deleteFilePath}`);
+});
--- run_scripts.sh
+++ run_scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-node a.js
-node b.js
-node c.js
-node d.js
+node a.js > a.log 2>&1 &
+# node b.js
+# node c.js
+# node d.js
