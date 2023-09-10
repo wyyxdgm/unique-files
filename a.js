9a859fcfbfb9fddb0fd643f5269a2b25b1681099
Damo <wyyxdgm@163.com>
2023-09-10 21:39:22 +0800

    sync

--- a.js
+++ a.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-let name = '我的资源';
+let name = '0-sightp.com';
 const folderPath = `/Volumes/dav_damo/baiduyun/${name}/`;
 // const folderPath = './folder';
 const outputFilePath = `a-${name}.json`;
