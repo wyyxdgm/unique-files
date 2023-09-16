4c012f2ecefdc91c5a3d34df03870646341449c7
Damo <wyyxdgm@163.com>
2023-09-16 18:05:03 +0800

    local:ast server

--- dav.js
+++ dav.js
@@ -8,7 +8,7 @@ const username = "admin";
 const password = "12345";
 let name = "baiduyun";
 let remoteBasePath = '/baiduyun';
-const webdavUrl = "http://192.168.3.100:5240/dav";
+const webdavUrl = "http://localhost:5219/dav";
 const client = createClient(webdavUrl, {
   username,
   password,
